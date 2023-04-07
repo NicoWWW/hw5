82421f2426691592861abd5b3d357b72ee414ab9
Nicole Wang <wangnico@usc.edu>
2023-04-07 05:46:13 +0000

    all done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(DailySchedule& sched, const AvailabilityMatrix& avail, const size_t maxShifts, const size_t dailyNeed);
+bool overworkCheck(unsigned int& worker, DailySchedule& sched, const size_t maxShifts); 
+Worker_T workerHelper(int& today, DailySchedule& sched); //function to select which worker to attempt to schedule
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +41,66 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    sched.emplace_back();
 
-
-
+    return scheduleHelper(sched, avail, maxShifts, dailyNeed);
 }
 
+bool scheduleHelper(DailySchedule& sched, const AvailabilityMatrix& avail, const size_t maxShifts, const size_t dailyNeed){
+    unsigned int daysToSchedule = avail.size();
+    unsigned int scheduledDays = sched.size();
+
+    if(scheduledDays > daysToSchedule){
+        sched.pop_back();
+        return true;
+    }
+
+    int today = sched.size()-1;
+
+    Worker_T currWorker = workerHelper(today, sched);
+
+    while(currWorker<avail[today].size()){ 
+			if(avail[today][currWorker] == 0 || overworkCheck(currWorker, sched, maxShifts) == true){ 
+					currWorker++;
+					continue;
+			}
+
+			sched[today].push_back(currWorker); 
+
+			if(sched[today].size()==dailyNeed){
+					sched.emplace_back();
+
+					if(scheduleHelper(sched, avail, maxShifts, dailyNeed)==true){
+							return true;
+					}
+					else{
+							sched.pop_back();
+					}
+			}
+			else if(scheduleHelper(sched, avail, maxShifts, dailyNeed)==true){
+					return true;
+			}
+			sched[today].pop_back();
+			currWorker++;
+    }
+    return false; 
+}
+
+bool overworkCheck(unsigned int& worker, DailySchedule& sched, const size_t maxShifts){
+    int count = 0;
+    for(unsigned int i=0; i<sched.size(); i++){
+        for(unsigned int j=0; j<sched[i].size(); j++){
+            if(sched[i][j]==worker){
+                count++;
+            }
+        }
+    }
+    return count >= (int)maxShifts;
+}
+
+Worker_T workerHelper(int& today, DailySchedule& sched){
+    if(sched[today].size()==0){
+        return 0;
+    }
+    return sched[sched.size()-1].back()+1;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -11,10 +11,9 @@
 #include "dict-eng.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
-
+std::set<std::string> wordleHelper(string& prefix, const string& in, const string& floating);
+ 
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +21,91 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> allCombos;
+		std::string prefix = "";
 
+		if (in == prefix){
+			return allCombos;
+		}
+
+		allCombos = wordleHelper(prefix, in, floating);
+
+		std::set<std::string> output;
+    for(std::set<std::string>::iterator it = allCombos.begin(); it != allCombos.end(); ++it){
+        if(dict.find(*it) != dict.end()){
+          output.insert(*it);
+        }
+    }
+
+		return output;
 }
 
 // Define any helper functions here
+std::set<std::string> wordleHelper(string& prefix, const string& in, const string& floating){
+	set<string> output;
+	
+	if((in.size()-prefix.size())==1){ 
+		if(floating.size()==1){ 
+			std::string temp = prefix + floating;
+			set<string> t = {temp};
+			return t;
+		}
+		else if(in[in.size()-1] != '-'){ 
+			std::string temp = prefix + in[in.size()-1];
+			std::set<std::string> t = {temp};
+			return t;
+		}
+		else{ 
+			std::set<std::string> newWord;
+			for(char letter = 'a'; letter <= 'z'; letter++){
+				std::string temp = prefix + letter;
+				newWord.insert(temp);
+			}
+			return newWord;
+		}
+	}
+
+	else{
+		unsigned int x = prefix.size(); 
+		unsigned int dashCount = 0;
+		while(x!=in.size()){
+			if(in[x]=='-'){
+				dashCount++;
+			}
+			x++;
+		}
+
+		if(in[prefix.size()] != '-'){
+			std::string temp = prefix + in[prefix.size()];
+			output = wordleHelper(temp, in, floating);
+		}
+
+		else if(floating.size() == dashCount){
+			for(unsigned int i = 0; i < floating.size(); i++){ 
+				std::string newFloat = floating.substr(0, i) + floating.substr(i+1);
+				std::string temp = prefix + floating[i];
+				std::set<std::string> results = wordleHelper(temp, in, newFloat);
+				output.insert(results.begin(), results.end());
+			}
+
+		}
+
+		else{
+			for(char letter = 'a'; letter <= 'z'; letter++){
+				if(floating.find(letter) == string::npos){ 
+					std::string temp = prefix + letter; 
+					std::set<std::string> results = wordleHelper(temp, in, floating);
+					output.insert(results.begin(), results.end());
+				}
+				else{ 
+					int index = floating.find(letter);
+					std::string newFloat = floating.substr(0, index) + floating.substr(index+1);
+					std::string temp = prefix + letter;
+					std::set<std::string> results = wordleHelper(temp, in, newFloat);
+					output.insert(results.begin(), results.end());
+				}
+			}
+		}
+	}
+	return output;
+} 
